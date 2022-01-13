7b5dcf5f6c387d39e34d040648d45d36f619044c
heinnssin <heinnssinp@gmail.com>
2022-01-13 01:15:22 -0500

    skill-form

--- src/components/SkillUpdate.js
+++ src/components/SkillUpdate.js
@@ -1,25 +1,42 @@
 import React, { useState, useEffect} from 'react'
 import { connect, useSelector, useDispatch } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { _loadSkills, _loadClients } from '../../store'
 
 const SkillUpdate =(props)=>{
   const { id } = useParams()
-  const [form, setForm] = useState()
-  const skills = useSelector((state) => state.skills);
-  const skill = useSelector((state) => state.skills.filter(skill => skill.id === id*1)[0]);
+  const [name, setName] = useState('')
   
+  const skill = useSelector((state, ownProps) => state.skills.filter(skill => skill.id === id*1)[0]);
+
   const dispatch = useDispatch();
   useEffect(()=>{
     dispatch(_loadSkills())
   },[])
+  const submit = (e) => {
+    e.preventDefault()
+    props.handleSubmit(post)
+  }
 
-  console.log('skill-->', skill)
+  //console.log('skill-->', skill)
   return (
     <div>
-      <h1>Acme Talent Agency</h1>
-      <form>
-        <input type='text' name='skillName' placeholder={skill ? skill.name : 'Skill name' } />
+      <Link to='/home'> 
+        <h1>Acme Talent Agency</h1>
+      </Link>
+      <form onSubmit={submit}>
+        <input 
+          type='text'
+          name='name' 
+          placeholder={skill ? skill.name : 'Skill name' } 
+          onChange={(evt)=> {
+            //console.log(name)
+            setName(evt.target.value)
+          }}
+          />
+        
+        <button type='submit' disabled={name.length === 0}>Update</button>
+        <button onClick={()=>{props.history.push('/home')}}>Cancel</button>
       </form>
     </div>
   )
@@ -27,19 +44,11 @@ const SkillUpdate =(props)=>{
 
 
 
-// const mapState = (state, ownProps)=>{
-//   console.log('ownProps',ownProps)
-//   const skill = state.skills.filter(skill => skill.id === ownProps.match.params.id*1)[0]
-//   //console.log('mapState Skill->', skill)
-//   return {
-//     skills: state.skills,
-//     //skill
-//   }
-// }
-// const mapDispatch = (dispatch)=>{
-//   return {
-//     loadSkills: ()=>{ dispatch(_loadSkills())},
-//   }
-// }
+const mapState = (state, ownProps)=>{
+  return {
+    history: ownProps.history
+    //skill
+  }
+}
 
-export default SkillUpdate
+export default connect(mapState)(SkillUpdate)
